dd55cd571fbf591eaab1e536d04af11fabe20fd6
Martanto <martanto@live.com>
2024-01-03 10:49:01 +0700

    add documentation for config

--- config/magma-trait.php
+++ config/magma-trait.php
@@ -1,6 +1,25 @@
 <?php
 
 return [
+    /*
+    |--------------------------------------------------------------------------
+    | User Providers
+    |--------------------------------------------------------------------------
+    |
+    | All authentication drivers have a user provider. This defines how the
+    | users are actually retrieved out of your database or other storage
+    | mechanisms used by this application to persist your user's data.
+    |
+    */
     'model' => config('auth.providers.users.model'),
+
+    /*
+    |--------------------------------------------------------------------------
+    | MAGMA API url
+    |--------------------------------------------------------------------------
+    |
+    | This one define where the MAGMA API url located
+    |
+    */
     'api_url' => 'https://magma.esdm.go.id/api',
 ];
